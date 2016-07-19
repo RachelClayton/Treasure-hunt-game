079c5c1c449e7afdedcf12efe78bf958174d0926
Varsha Prem <vpicequeen@gmail.com>
2016-07-19 10:17:54 -0400

    Treasure game first commit

---
+++ Assets/scripts/endhint.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class endhint : MonoBehaviour {
+	Collider2D box;
+	public GameObject player;
+	Collider2D playerbox;
+	public Text UItext;
+
+	// Use this for initialization
+	void Start () {
+		box = GetComponent<Collider2D> ();
+		playerbox = player.GetComponent<Collider2D> ();
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (playerbox.IsTouching (box)) {
+			UItext.text = " ";
+		}
+
+
+	}
+
+}
---
+++ Assets/scripts/hints.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class hints : MonoBehaviour {
+	Collider2D box;
+	public GameObject dest;
+	public bool killblock;
+	public bool zoom;
+	public GameObject player;
+	Collider2D playerbox;
+	public Text UItext;
+	public string hint;
+
+
+	public string changetext;
+
+
+	// Use this for initialization
+	void Start () {
+		box = GetComponent<Collider2D> ();
+		playerbox = player.GetComponent<Collider2D> ();
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerbox.IsTouching (box)) {
+			UItext.text = hint;
+			if (killblock = true) {
+				Destroy (dest);
+			}
+		}
+
+		
+	}
+
+}
